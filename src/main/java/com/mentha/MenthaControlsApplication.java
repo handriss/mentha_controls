ae33d4530009334bddf052da36b35c49ad75f40e
handriss <andrashinkel@gmail.com>
2017-04-07 17:35:25 +0200

    Initial commit

---
+++ src/main/java/com/mentha/MenthaControlsApplication.java
@@ -0,0 +1,12 @@
+package com.mentha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenthaControlsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenthaControlsApplication.class, args);
+	}
+}
---
+++ src/main/java/com/mentha/controller/PortController.java
@@ -0,0 +1,15 @@
+package com.mentha.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PortController {
+
+    @RequestMapping
+    public String home(){
+        return "index";
+    }
+
+}
