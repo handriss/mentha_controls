18a532d0dcd9f3b4be5b568788f3daea031cd67c
lombocska <lombos.monika@gmail.com>
2017-04-30 11:16:26 +0200

    SH-5 created 'change' boolean field

--- src/main/java/com/mentha/controller/StatusController.java
+++ src/main/java/com/mentha/controller/StatusController.java
@@ -1,5 +1,7 @@
 package com.mentha.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class StatusController {
 
     @GetMapping
-    public String checkStatus(){
-        return "Application is up and running.";
+    public ResponseEntity checkStatus(){
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
     }
 }
--- src/main/java/com/mentha/model/Ports.java
+++ src/main/java/com/mentha/model/Ports.java
@@ -14,35 +14,39 @@ import javax.persistence.Id;
 @Entity
 public class Ports {
 
-  private String nev;
-  private Double adat;
-  private String miez;
-  private String fajta;
-  private String szoveg;
+    private String nev;
+    private Double adat;
+    private String miez;
+    private String fajta;
+    private String szoveg;
 
-  @Id
-  @GeneratedValue
-  private Long id;
-  private java.sql.Timestamp ido;
-  private String icon0;
-  private Long value1;
-  private Long min1;
-  private Long max1;
-  private String icon1;
-  private Long x1;
-  private Long y1;
-  private Long value2;
-  private Long min2;
-  private Long max2;
-  private String icon2;
-  private Long x2;
-  private Long y2;
-  private Long value3;
-  private Long min3;
-  private Long max3;
-  private String icon3;
-  private Long x3;
-  private Long y3;
+    @Id
+    @GeneratedValue
+    private Long id;
+    private java.sql.Timestamp ido;
+    private String icon0;
+    private Long value1;
+    private Long min1;
+    private Long max1;
+    private String icon1;
+    private Long x1;
+    private Long y1;
+    private Long value2;
+    private Long min2;
+    private Long max2;
+    private String icon2;
+    private Long x2;
+    private Long y2;
+    private Long value3;
+    private Long min3;
+    private Long max3;
+    private String icon3;
+    private Long x3;
+    private Long y3;
 
-  public Ports(){};
+    private Boolean change;
+
+    public Ports() {
+        this.change = false;
+    }
 }
